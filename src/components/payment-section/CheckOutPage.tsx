1c511b8b5c475687f85149124d381509b9127275
devanshu-crescentek <devanshu@outrightsolutions.net>
2025-02-21 17:53:37 +0530

    design fixed

--- src/components/payment-section/CheckOutPage.tsx
+++ src/components/payment-section/CheckOutPage.tsx
@@ -1,22 +1,19 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 'use client'
 
-import { useEffect, useState } from 'react'
-import { useRouter } from 'next/navigation'
-import { FaPaypal, FaStripe } from 'react-icons/fa'
-import { RiVisaLine, RiMastercardFill } from 'react-icons/ri'
-import { SiAmericanexpress } from 'react-icons/si'
+import useDeviceType from '@/hooks/use-device-type'
+import convertToSubCurrency from '@/lib/convertToSubCurrency'
 import {
-  useStripe,
-  useElements,
   PaymentElement,
   PaymentRequestButtonElement,
+  useElements,
+  useStripe,
 } from '@stripe/react-stripe-js'
-import convertToSubCurrency from '@/lib/convertToSubCurrency'
+import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
 import PaypalButton from './paypal-button'
-import { FaApplePay, FaGooglePay } from 'react-icons/fa6'
-import useDeviceType from '@/hooks/use-device-type'
 
+import { Button } from '@/components/ui/button'
 import {
   Drawer,
   DrawerClose,
@@ -27,7 +24,7 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from '@/components/ui/drawer'
-import { Button } from '@/components/ui/button'
+import Image from 'next/image'
 
 const CheckOutPage = ({ amount }: { amount: number }) => {
   const stripe = useStripe()
@@ -186,37 +183,125 @@ const CheckOutPage = ({ amount }: { amount: number }) => {
         </div>
       </form>
       <hr className='my-4 border-gray-300' />
-      <div className='flex items-center justify-between gap-11'>
+      <div className='flex items-center justify-between gap-6 mb-40'>
         {/* Secure Payment Text */}
-        <h2 className='text-lg font-semibold'>Secure Payment</h2>
+        <h2 className='md:text-[20px] text-[15px] font-semibold whitespace-nowrap'>
+          Secure Payment
+        </h2>
 
         {/* Payment Icons */}
-        <div className='flex gap-2 flex-wrap'>
+        <div className='flex flex-wrap items-center justify-start gap-1'>
           <div className='px-2 py-1 bg-[#e9e9e9] text-[#9c9c9c] rounded'>
-            <FaPaypal className='text-[1.3rem]' />
+            {/* <FaPaypal className='text-[1.3rem]' /> */}
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='7.056000232696533 3 37.35095977783203 45'
+              className='w-[30px] h-[12px]'
+            >
+              <g xmlns='http://www.w3.org/2000/svg' clip-path='url(#a)'>
+                <path
+                  fill='#002991'
+                  d='M38.914 13.35c0 5.574-5.144 12.15-12.927 12.15H18.49l-.368 2.322L16.373 39H7.056l5.605-36h15.095c5.083 0 9.082 2.833 10.555 6.77a9.687 9.687 0 0 1 .603 3.58z'
+                ></path>
+                <path
+                  fill='#60CDFF'
+                  d='M44.284 23.7A12.894 12.894 0 0 1 31.53 34.5h-5.206L24.157 48H14.89l1.483-9 1.75-11.178.367-2.322h7.497c7.773 0 12.927-6.576 12.927-12.15 3.825 1.974 6.055 5.963 5.37 10.35z'
+                ></path>
+                <path
+                  fill='#008CFF'
+                  d='M38.914 13.35C37.31 12.511 35.365 12 33.248 12h-12.64L18.49 25.5h7.497c7.773 0 12.927-6.576 12.927-12.15z'
+                ></path>
+              </g>
+            </svg>
           </div>
           <div className='px-2 py-1 bg-[#e9e9e9] text-[#9c9c9c] rounded'>
-            <FaStripe className='text-[1.3rem]' />
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='w-[30px] h-[12px]'
+              viewBox='0 0 512 214'
+            >
+              <path
+                fill='#635BFF'
+                d='M512 110.08c0-36.409-17.636-65.138-51.342-65.138c-33.85 0-54.33 28.73-54.33 64.854c0 42.808 24.179 64.426 58.88 64.426c16.925 0 29.725-3.84 39.396-9.244v-28.445c-9.67 4.836-20.764 7.823-34.844 7.823c-13.796 0-26.027-4.836-27.591-21.618h69.547c0-1.85.284-9.245.284-12.658m-70.258-13.511c0-16.071 9.814-22.756 18.774-22.756c8.675 0 17.92 6.685 17.92 22.756zm-90.31-51.627c-13.939 0-22.899 6.542-27.876 11.094l-1.85-8.818h-31.288v165.83l35.555-7.537l.143-40.249c5.12 3.698 12.657 8.96 25.173 8.96c25.458 0 48.64-20.48 48.64-65.564c-.142-41.245-23.609-63.716-48.498-63.716m-8.534 97.991c-8.391 0-13.37-2.986-16.782-6.684l-.143-52.765c3.698-4.124 8.818-6.968 16.925-6.968c12.942 0 21.902 14.506 21.902 33.137c0 19.058-8.818 33.28-21.902 33.28M241.493 36.551l35.698-7.68V0l-35.698 7.538zm0 10.809h35.698v124.444h-35.698zm-38.257 10.524L200.96 47.36h-30.72v124.444h35.556V87.467c8.39-10.951 22.613-8.96 27.022-7.396V47.36c-4.551-1.707-21.191-4.836-29.582 10.524m-71.112-41.386l-34.702 7.395l-.142 113.92c0 21.05 15.787 36.551 36.836 36.551c11.662 0 20.195-2.133 24.888-4.693V140.8c-4.55 1.849-27.022 8.391-27.022-12.658V77.653h27.022V47.36h-27.022zM35.982 83.484c0-5.546 4.551-7.68 12.09-7.68c10.808 0 24.461 3.272 35.27 9.103V51.484c-11.804-4.693-23.466-6.542-35.27-6.542C19.2 44.942 0 60.018 0 85.192c0 39.252 54.044 32.995 54.044 49.92c0 6.541-5.688 8.675-13.653 8.675c-11.804 0-26.88-4.836-38.827-11.378v33.849c13.227 5.689 26.596 8.106 38.827 8.106c29.582 0 49.92-14.648 49.92-40.106c-.142-42.382-54.329-34.845-54.329-50.774'
+              />
+            </svg>
           </div>
           <div className='px-2 py-1 bg-[#e9e9e9] text-[#9c9c9c] rounded'>
-            <RiVisaLine className='text-[1.3rem]' />
+            <svg
+              width='30'
+              height='12'
+              viewBox='0 0 30 12'
+              xmlns='http://www.w3.org/2000/svg'
+              xmlnsXlink='http://www.w3.org/1999/xlink'
+            >
+              <g>
+                <g>
+                  <path
+                    d='M12.4,4.3L11.3,10h2.2l1.1-5.7H12.4z M9.1,4.3L7.1,9l-0.2-1l-0.1-0.5
+               C6.2,6.8,5.5,5.6,4.2,4.5c-0.3-0.2-0.5-0.3-0.8-0.5l1.9,7.1h2.2l3.4-6.8H9.1z 
+               M17.7,6.6c0-0.9,2.1-0.8,3-0.3l0.3-1.8c0,0-0.9-0.3-1.9-0.3c-1,0-3.6,0.5-3.6,2.8
+               c0,2.2,3,2.2,3,3.3c0,1.1-2.7,0.9-3.6,0.2l-0.3,1.8c0,0,1,0.5,2.5,0.5c1.5,0,3.7-0.8,3.7-2.8
+               C20.7,7.8,17.7,7.6,17.7,6.6z 
+               M26.7,4.3h-1.7c-0.8,0-1,0.6-1,0.6l-3.2,6.5h2.2l0.5-1.2h2.7l0.2,1.2h2L26.7,4.3z 
+               M24,7.9l1.1-3l0.6,3H24z'
+                    fillRule='evenodd'
+                    clipRule='evenodd'
+                    fill='#005BAC'
+                  />
+                </g>
+              </g>
+              <g>
+                <g>
+                  <path
+                    d='M5.6,5.2c0,0-0.1-0.7-1-0.7H1.4l0,0.1c0,0,1.6,0.3,3.2,1.6c1.5,1.2,2,2.7,2,2.7L5.6,5.2z'
+                    fillRule='evenodd'
+                    clipRule='evenodd'
+                    fill='#F6AC1D'
+                  />
+                </g>
+              </g>
+            </svg>
           </div>
           <div className='px-2 py-1 bg-[#e9e9e9] text-[#9c9c9c] rounded'>
-            <RiMastercardFill className='text-[1.3rem]' />
+            {/* <RiMastercardFill className='text-[1.3rem]' /> */}
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              width='30'
+              height='12'
+              viewBox='0 0 30 12'
+            >
+              <g fill='none' fillRule='evenodd'>
+                <circle cx='9' cy='6' r='6' fill='#ea001b' />
+                <circle cx='17' cy='6' r='6' fill='#ffa200' fillOpacity='0.8' />
+              </g>
+            </svg>
           </div>
           <div className='px-2 py-1 bg-[#e9e9e9] text-[#9c9c9c] rounded'>
-            <SiAmericanexpress className='text-[1.3rem]' />
+            <Image
+              width={30}
+              height={10}
+              src={'/a-express.png'}
+              alt='american-express'
+            />
           </div>
           <div className='px-2 py-1 bg-[#e9e9e9] text-[#9c9c9c] rounded'>
-            <FaApplePay className='text-[1.3rem]' />
+            <Image
+              width={30}
+              height={10}
+              src={'/apple-pay.png'}
+              alt='Apple Pay'
+            />
           </div>
           <div className='px-2 py-1 bg-[#e9e9e9] text-[#9c9c9c] rounded'>
-            <FaGooglePay className='text-[1.3rem]' />
+            <Image width={30} height={12} src={'/g-pay.png'} alt='gpay' />
+          </div>
+          <div className='px-2 py-1 bg-[#e9e9e9] text-[#9c9c9c] rounded h-[22px] flex items-center justify-center'>
+            <Image width={30} height={100} src={'/card.png'} alt='Card' />
           </div>
         </div>
       </div>
       {paymentRequestAvailable && paymentRequest && deviceType === 'mobile' && (
-        <div className='fixed block md:hidden bottom-0 left-0 w-full border shadow-[0px_-2px_4px_0px_rgba(0,0,0,0.12)] rounded-t-xl p-4'>
+        <div className='fixed block md:hidden bottom-0 left-0 w-full border shadow-[0px_-2px_4px_0px_rgba(0,0,0,0.12)] rounded-t-xl p-4 bg-white'>
           {/* Payment Methods */}
           <div className='flex justify-between space-x-3 mb-3'>
             <div className='w-full'>
--- src/components/payment-section/index.tsx
+++ src/components/payment-section/index.tsx
@@ -1,11 +1,20 @@
 'use client'
+import Image from 'next/image'
 import { useState } from 'react'
 
 import { Elements } from '@stripe/react-stripe-js'
 import { loadStripe } from '@stripe/stripe-js'
-import { Check } from 'lucide-react'
+import { Check, Edit, Info } from 'lucide-react'
 import convertToSubCurrency from '@/lib/convertToSubCurrency'
 import CheckOutPage from './CheckOutPage'
+import { Button } from '../ui/button'
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip'
 
 const stripePromise = loadStripe(
   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
@@ -17,13 +26,24 @@ const documents = [
     name: 'Title Register',
     price: 19.95,
     required: true,
+    description:
+      'Shows property ownership, description, and any restrictive covenants. For a visual of the land, consider purchasing the Title Plan.',
   },
-  { id: 'title-plan', name: 'Title Plan', price: 19.95, required: false },
+  {
+    id: 'title-plan',
+    name: 'Title Plan',
+    price: 19.95,
+    required: false,
+    description:
+      'A visual representation of the property or land in a registered title. Consider purchasing the Title Register for details.',
+  },
   {
     id: 'conveyancing-pack',
     name: 'Conveyancing Pack',
     price: 49.95,
     required: false,
+    description:
+      'The pack includes all available Lease Deeds, Transfer Deeds, Conveyancing Deeds, and Charges for the selected property.',
   },
 ]
 
@@ -50,53 +70,121 @@ const PaymentSection = () => {
     <div>
       <div className='w-full py-10 sm:py-20'>
         <div className='container max-w-6xl mx-auto'>
-          <div className='grid lg:grid-cols-2  md:gap-10 gap-3  md:px-10 px-5'>
+          <div className='grid lg:grid-cols-2 md:gap-10 gap-3 md:px-10 px-5'>
             <div className=''>
-              <div className='flex items-start space-x-2'>
-                <span className='text-2xl'>👇</span>
-                <h2 className='text-2xl font-semibold'>
+              <div className='flex items-center space-x-2 mb-4'>
+                <Image src='/thumb.svg' alt='Check' width={30} height={30} />
+                <h2 className='md:text-2xl text-[18px] font-semibold'>
                   We&apos;ve found the following documents for the address
                 </h2>
               </div>
 
               {/* Address */}
-              <p className='text-gray-600 text-sm mt-2'>
-                24 Boughton Rd, Wick Hill’ CA, RG40 9BL
-                <br />
-                <span className='font-medium'>Title Number: 13456</span>
-              </p>
+              <div className='flex items-start justify-between mb-6'>
+                <p className='text-[#0B0C0C] md:text-[20px] text-[18px] leading-[30px]'>
+                  24 Boughton Rd,
+                  <br className='md:hidden block' />
+                  Wick Hill’ CA,
+                  <br className='md:hidden block' />
+                  RG40 9BL
+                  <br />
+                  Title Number: 13456
+                </p>
+                <Button className='bg-[#28A745] rounded-[4px] ' disabled>
+                  <Edit /> Edit
+                </Button>
+              </div>
 
-              <hr className='my-4 border-gray-300' />
+              <div className='border-t border-dashed border-[#000000] my-4'></div>
 
               {/* Document Selection */}
-              <h3 className='font-semibold'>Select Your Documents</h3>
+              <h3 className='font-semibold md:text-[30px] text-[18px] leading-[23px] mt-6'>
+                Select Your Documents
+              </h3>
 
-              <div className='mt-3 space-y-3'>
+              <div className='mt-3 space-y-3 hidden md:block'>
                 {documents.map((doc) => (
                   <div
                     key={doc.id}
                     className={`p-3 rounded-md flex items-start space-x-3 cursor-pointer`}
                     onClick={() => !doc.required && toggleSelection(doc.id)}
                   >
-                    <div className='w-5 h-5 flex items-center justify-center border rounded-md bg-white'>
+                    <div
+                      className={`min-w-[27px] w-[27px] min-h-[27px] h-[27px] flex items-center justify-center border rounded-md ${
+                        selectedDocs.includes(doc.id)
+                          ? 'bg-[#28A745] text-white'
+                          : 'bg-white'
+                      }`}
+                    >
                       {selectedDocs.includes(doc.id) && (
-                        <Check size={16} className='text-green-500' />
+                        <Check size={20} className='text-white' />
                       )}
                     </div>
                     <div>
                       <p className='font-semibold'>
                         {doc.name} - £{doc.price.toFixed(2)}
                       </p>
+                      <p className='text-sm text-gray-600'>{doc.description}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+              <div className='mt space-y-3 block md:hidden'>
+                {documents.map((doc) => (
+                  <div
+                    key={doc.id}
+                    className={`py-3 rounded-md flex items-start space-x-3 cursor-pointer`}
+                    onClick={() => !doc.required && toggleSelection(doc.id)}
+                  >
+                    <div className='flex flex-col items-start justify-between w-full'>
+                      <p className='font-semibold text-[18px] mb-2'>
+                        {doc.name}
+                      </p>
+                      <p className='text-sm text-gray-600'>{doc.description}</p>
+                    </div>
+                    <p className='font-semibold text-[20px]'>
+                      £{doc.price.toFixed(2)}
+                    </p>
+                    <div
+                      className={`min-w-[20px] w-[20px] min-h-[20px] h-[20px] flex items-center justify-center border rounded-md mt-1 ${
+                        selectedDocs.includes(doc.id)
+                          ? 'bg-[#28A745] text-white'
+                          : 'bg-white'
+                      }`}
+                    >
+                      {selectedDocs.includes(doc.id) && (
+                        <Check size={16} className='text-white' />
+                      )}
                     </div>
                   </div>
                 ))}
               </div>
 
-              <hr className='my-4 border-gray-300' />
+              <div className='border-t border-dashed border-[#000000] my-4'></div>
 
               {/* Fast Track Toggle */}
-              <div className='flex items-center justify-between'>
-                <p className='font-semibold'>Fast Track (1 Hour) - £10.00</p>
+              <div className='flex items-start justify-between'>
+                <div className=''>
+                  <p className='font-semibold flex items-center gap-1 text-[18px] leading-[30px]'>
+                    Fast Track (1 Hour){' '}
+                    <span className='hidden md:block'>- £10.00</span>
+                    <TooltipProvider>
+                      <Tooltip>
+                        <TooltipTrigger asChild>
+                          <Info
+                            size={16}
+                            className='text-[#000000] cursor-pointer'
+                          />
+                        </TooltipTrigger>
+                        <TooltipContent>
+                          <p>Fast Track processing fee</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                  </p>
+                  <p className='font-semibold flex items-center gap-1 text-[18px] leading-[30px] md:hidden'> £10.00</p>
+                </div>
+
                 <div
                   className={`${
                     isToggle ? '!bg-[#28A745]' : 'bg-[#f0f0f0]'
